34894075f72de9396a2a4dd726b2c0cde4f2d964
manuel-chinchi <manuel.chinchi93@gmail.com>
2022-06-19 19:42:28 -0300

    update setup.py - Fix for installation on windows systems

--- jq/setup.py
+++ jq/setup.py
@@ -1,4 +1,4 @@
-# Script to install jq
+# Script to install jq.exe
 # 
 # install 'pip'
 #   `curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py`
@@ -18,17 +18,17 @@ EXECUTABLE_FULL_NAME = EXECUTABLE_PATH + r"\jq.exe"
 def mkdir(new_dir):
     if os.path.exists(new_dir) == False:
         os.mkdir(new_dir)
-        print(">> Created new directory \"{}\" in system!!!".format(new_dir))
+        print(">> New directory (\"{}\") created in you system!!!".format(new_dir))
     else:
-        print(">> Directory \"{}\" already exists!!!".format(new_dir))
+        print(">> EXCEPTION: The directory \"{}\" already exists!!!".format(new_dir))
 
 def get_url_content(url, full_name):
     response = requests.get(url)
     if os.path.exists(full_name) == False:
         open(full_name, "wb").write(response.content)
-        print(">> Downloaded file in \"{}\" from {}!!!".format(full_name, url))
+        print(">> File \"jq.exe\" downloaded in directory (\"{}\")!!!".format(EXECUTABLE_PATH))
     else:
-        print(">> File \"{}\" already exists!!!".format(full_name))
+        print(">> EXCEPTION: The file \"jq.exe\" already exists in \"{}\"!!!".format(EXECUTABLE_PATH))
 
 # @author Kieran Wood
 # @ref https://stackoverflow.com/questions/63782773/how-to-modify-windows-10-path-variable-directly-from-a-python-script
@@ -40,25 +40,45 @@ def add_to_path(program_path: str):
 
         with winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER) as root: # Get the current user registry
             with winreg.OpenKey(root, "Environment", 0, winreg.KEY_ALL_ACCESS) as key: # Go to the environment key
-                existing_path_value = winreg.EnumValue(key, 3)[1] # Grab the current path value
-                new_path_value = existing_path_value + ";" + program_path + ";" # Takes the current path value and appends the new program path
+                current_path_user = winreg.EnumValue(key, 3)[1] # Grab the current path value
+                if current_path_user.find(program_path) > -1:
+                    print(">> EXCEPTION: The user variable PATH already contains the value \"{}\"!!!".format(program_path))
+                    return
+                if current_path_user[-1] != ";":
+                    current_path_user = current_path_user + ";"
+                new_path_value = current_path_user + program_path + ";" # Takes the current path value and appends the new program path
                 winreg.SetValueEx(key, "PATH", 0, winreg.REG_EXPAND_SZ, new_path_value) # Updated the path with the updated path
+
+        # This part allows the modified variable "PATH" to be recognized without the need to restart the computer.
+        HWND_BROADCAST = 0xFFFF
+        WM_SETTINGCHANGE = 0x1A
+        SMTO_ABORTIFHUNG = 0x0002
+        result = ctypes.c_long()
+        SendMessageTimeoutW = ctypes.windll.user32.SendMessageTimeoutW
+        SendMessageTimeoutW(HWND_BROADCAST, WM_SETTINGCHANGE, 0, u"Environment", SMTO_ABORTIFHUNG, 5000, ctypes.byref(result),)
     else: # If system is *nix
         with open(f"{os.getenv('HOME')}/.bashrc", "a") as bash_file:  # Open bashrc file
             bash_file.write(f'\nexport PATH="{program_path}:$PATH"\n')  # Add program path to Path variable
         os.system(f". {os.getenv('HOME')}/.bashrc")  # Update bash source
+    print(">> The user variable PATH has updated!!!")
     # print(f"Added {program_path} to path, please restart shell for changes to take effect")
 
+
 def setup():
-    mkdir(EXECUTABLE_PATH)
-    get_url_content(EXECUTABLE_URL, EXECUTABLE_FULL_NAME)
-    #TODO falta chequear si ya existe la variable de usuario
-    #TODO revisar 'add_to_path', ya que setea bien el valor pero borra el PATH original al parecer 
-    opt = input(">> Edit PATH enviroment system? y/n:\n")
-    if opt == 'y':
+    print("+--------------------------------+")
+    print("|                                |")
+    print("|   Setup installer for JQ 1.6   |")
+    print("|                                |")
+    print("+--------------------------------+")
+    print("")
+    opt = input( \
+        "The following script will create a directory on your system and modify "\
+        "the PATH environment variable (for users only) do you want to continue? (y/n)"\
+        "\n"
+        )
+    if opt.lower() == 'y':
+        mkdir(EXECUTABLE_PATH)
+        get_url_content(EXECUTABLE_URL, EXECUTABLE_FULL_NAME)
         add_to_path(EXECUTABLE_PATH)
-        print(">> Add \"{}\" into PATH system!!!".format(EXECUTABLE_PATH))
-    else:
-        print(">> Exit")
 
 setup()
